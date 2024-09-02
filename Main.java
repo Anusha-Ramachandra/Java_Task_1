5bbc2accb578db1bf0e24782ad31fd57f74a5027
Anusha R <95996727+Anusha-Ramachandra@users.noreply.github.com>
2024-09-02 18:49:54 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+  import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
